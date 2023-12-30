d0070343767d0740458fd56660a7fa4041b99201
samxal <sh.samxal>
2023-12-30 12:44:51 +0400

    adding indexes to watchlist

--- UserManagement/Watchlist.java
+++ UserManagement/Watchlist.java
@@ -1,26 +1,57 @@
 package UserManagement;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import MovieManagement.Movie;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Watchlist {
-    private ArrayList<Movie> watchList;
+    private Set<Integer> watchList;
 
     Watchlist() {
-        this.watchList = new ArrayList<>();
+        this.watchList = new HashSet<>();
     }
 
-    void addToWatchList(Movie m) {
-        watchList.add(m);
+    Watchlist(Set<Integer> watchList) {
+        this.watchList = watchList;
     }
 
-    void addToWatchList(List<Movie> movies) {
-        movies.stream().forEach(m -> watchList.add(m));
+    // Getters
+    public Set<Integer> getSet(){
+        return this.watchList;
     }
 
-    void removeByIndex(int index) {
-        watchList.remove(index);
+    // Setters
+    public void setWatchList(Set<Integer> idSet) {
+        this.watchList = idSet;
+    }
+
+    public void addToWatchList(int i) {
+        watchList.add(i);
+    }
+
+    public void addToWatchList(Set<Integer> idSet) {
+        watchList.addAll(idSet);
+    }
+
+    public void addToWatchList(String[] idList) {
+        for (String movieString : idList) {
+            try {
+                int movieId = Integer.parseInt(movieString.trim());
+                watchList.add(movieId);
+            } catch (NumberFormatException e) {
+                System.err.println("Error parsing '" + movieString + "' to an integer");
+            }
+        }
+    }
+
+    public void removeFromList(Integer id) {
+        watchList.remove(id);
+    }
+
+    @Override
+    public String toString() {
+        return watchList.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(";"));
     }
 }
