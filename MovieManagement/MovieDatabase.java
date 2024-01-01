e29316c5bbb860f5298aeeb64a094b4b77ec95a1
samxal <sh.samxal>
2024-01-01 18:16:30 +0400

    adding id to movies

--- MovieManagement/MovieDatabase.java
+++ MovieManagement/MovieDatabase.java
@@ -10,14 +10,16 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class MovieDatabase  {
+public class MovieDatabase {
     private static String filepath = "Resources/movies.csv";
     static List<Movie> movies = loadMoviesFromFile();
 
     public static void addToFile(Movie m) {
+        m.setId(movies.size());
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath, true))) {
             writer.write(m.toString());
             writer.newLine();
+            movies.add(m);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -41,7 +43,8 @@ public class MovieDatabase  {
             while ((line = br.readLine()) != null) {
                 String[] data = line.split(",");
                 if (data.length >= 4) {
-                    Movie movie = new Movie(data[0], data[2], Integer.parseInt(data[1]), Integer.parseInt(data[3]));
+                    Movie movie = new Movie(Integer.parseInt(data[0]), data[1], data[2], Integer.parseInt(data[3]),
+                            Integer.parseInt(data[4]));
                     resultMovies.add(movie);
                 }
             }
@@ -59,7 +62,7 @@ public class MovieDatabase  {
 
     public static List<Movie> getMoviesByIndex(Set<Integer> movieIDs) {
         List<Movie> resMovies = new ArrayList<>();
-        movieIDs.stream().filter(i->i > 0 && i < movies.size()).forEach(i->resMovies.add(movies.get(i)));
+        movieIDs.stream().filter(i -> i > 0 && i < movies.size()).forEach(i -> resMovies.add(movies.get(i)));
         return resMovies;
     }
 
